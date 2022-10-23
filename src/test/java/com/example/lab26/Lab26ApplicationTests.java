2d21a875fffdc92415f5f4971ddda160fa62dc28
feedblackg44 <pilmyh@gmail.com>
2022-10-23 18:42:59 +0300

    proj init

---
+++ src/test/java/com/example/lab26/Lab26ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab26;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab26ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
